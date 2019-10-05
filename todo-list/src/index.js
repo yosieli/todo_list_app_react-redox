26d234152dc7b7a58dbf2a2c80127077d9db0c56
feven tsegay <feventsegay6@gmail.com>
2019-10-05 11:25:23 -0500

    first commit

---
+++ todo-list/src/index.js
@@ -0,0 +1,18 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers/store'
+
+const store = createStore(reducer)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
---
+++ todo-list/src/reducers/store.js
@@ -0,0 +1,20 @@
+ const initialState = {
+     todo: ''
+ }
+const reducer =  (state = initialState, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+        let newState = [...state, action.value]
+        return newState
+        case 'REMOVE_TODO':
+        return[
+        ...state.slice(0,action.value),
+        ...state.slice(action.value + 1, state.length -1)
+        ]
+        default:
+        return state
+
+    }
+
+}
+export default reducer
